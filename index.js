44e8e7eaa2195cc29e20281a6927a8885f594543
JustinSingleton <jsingle9@msudenver.edu>
2021-03-11 15:54:08 -0700

    added a call to supervillain npm package

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const fs = require("fs");
 var superheroes = require("superheroes");
 //fs.copyFileSync("a.file", "b.file");
+var supervillains = require("supervillains");
 
 var j = superheroes.random();
+var k = supervillains.random();
 console.log(j);
+console.log(k);
